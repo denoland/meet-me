595de992296fb3f083b2cf36df248ed05839230a
Yoshiya Hinosawa <stibium121@gmail.com>
2022-05-02 13:34:13 +0900

    chore: tweak layout

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@
 
 export function Footer() {
   return (
-    <footer className="h-40 flex items-center justify-center bg-gray-600 text-white">
+    <footer className="mt-40 h-40 flex items-center justify-center bg-gray-600 text-white">
       <span className="">© 2018-2022 the Deno authors.</span>
     </footer>
   );
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 export default function LandingPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center py-36">
       <button className="border border-1 rounded px-3">TODO(kt3k): Sign in with google</button>
     </div>
 );
