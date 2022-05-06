a54021bf0e21eb4148a1a0c0f7fe0bf721b1aad8
Yoshiya Hinosawa <stibium121@gmail.com>
2022-05-06 22:15:32 +0900

    refactor: create utils/jwt.ts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 import { useEffect, useRef } from "react";
 import { useForwardProps } from "aleph/react";
+import { parsePayload } from "utils/jwt.ts";
 
 export default function LandingPage() {
   const { clientId } = useForwardProps<{ clientId: string }>();
@@ -9,14 +10,7 @@ export default function LandingPage() {
     google.accounts.id.initialize({
       client_id: clientId,
       callback: (res) => {
-        const jwt = res.credential;
-        const base64 = jwt.split(".")[1].replace(/-/g, "+").replace(/_/g, "/");
-        const json = decodeURIComponent(
-          atob(base64).split("").map(
-            (c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2),
-          ).join(""),
-        );
-        const payload = JSON.parse(json);
+        const payload = parsePayload(res.credential);
         alert(`Successfully signed in as ${payload.email} (${payload.name})!`);
       },
     });
