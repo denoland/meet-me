2719d6a861d48f1042e5011d22b7c93575437c2a
Yoshiya Hinosawa <stibium121@gmail.com>
2022-05-07 17:10:06 +0900

    feat: add privcy and terms placeholders

---
+++ routes/privacy.tsx
@@ -0,0 +1,7 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+export default function PrivacyPolicy() {
+  return (
+    <div>Privacy Policy</div>
+  )
+}
---
+++ routes/terms.tsx
@@ -0,0 +1,7 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+
+export default function TermsOfService() {
+  return (
+    <div>Terms of service</div>
+  )
+}
