964b22e4084fac03c6f6989a5881a3e1536a0dff
Yoshiya Hinosawa <stibium121@gmail.com>
2022-05-27 00:03:48 +0900

    wip implement slug update, add onboarding panel transition

--- routes/mypage/onboarding.tsx
+++ routes/mypage/onboarding.tsx
@@ -4,10 +4,15 @@ import { useForwardProps, useRouter } from "aleph/react";
 import { UserForClient } from "utils/db.ts";
 import Button from "base/Button.tsx";
 import icons from "icons";
+import cx from "utils/cx.ts";
+
+type Step = "slug" | "availability";
 
 export default function OnboardingPage() {
   const { user } = useForwardProps<{ user: UserForClient }>();
+  console.log("user", user);
   const { redirect } = useRouter();
+  const [step, setStep] = useState<Step>("slug");
   useEffect(() => {
     if (!user) {
       redirect("/");
@@ -20,25 +25,54 @@ export default function OnboardingPage() {
 
   return (
     <div className="px-8 py-4">
-      <ChooseURL />
+      {step === "slug" && <ChooseURL onFinish={() => setStep("availability")} />}
+      {step === "availability" && <ChooseAvailabilities />}
     </div>
   );
 }
 
-function ChooseURL() {
+function ChooseURL({ onFinish }: {onFinish: () => void}) {
   const [slug, setSlug] = useState("");
-  const [a, setA] = useState(true);
+  const [fadingOut, setFadingOut] = useState(false);
+  const [updating, setUpdating] = useState(false);
+
+  const updateSlug = async () => {
+    setUpdating(true);
+    try {
+      // TODO(kt3k): Use some API updating utility
+      const res = await fetch("/api/user", {
+        method: "PATCH",
+        body: JSON.stringify({ slug })
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message)
+      }
+      setFadingOut(true);
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+      onFinish();
+    } catch (e) {
+      alert(e.message);
+    } finally {
+      setUpdating(false);
+    }
+  }
+
   return (
     <div
-      className="absolute w-screen transition-500"
-      style={{ opacity: a ? "1" : "0" }}
+      className={cx("absolute w-screen transition-500", {
+        "opacity-0": fadingOut,
+        "!-translate-x-5": fadingOut,
+      })}
     >
       <div className="max-w-md mx-auto">
         <h1 className="text-lg">
           Welcome to <span className="font-semibold">Meet Me</span>!
         </h1>
       </div>
-      <div className="mt-8 flex flex-col gap-4 border rounded-lg border-gray-300 py-8 px-8 max-w-lg mx-auto">
+      <div className="mt-8 flex flex-col gap-4 border rounded-lg border-gray-600 py-8 px-8 max-w-lg mx-auto">
         <h2 className="font-medium text-lg">Create your Meet Me URL</h2>
         <p className="text-gray-500">
           Choose a URL that describes you or your business in a concise way.
@@ -57,9 +91,9 @@ function ChooseURL() {
         </div>
         <div className="self-end mt-4">
           <Button
-            disabled={slug === ""}
+            disabled={slug === "" || updating}
             style="primary"
-            onClick={() => setA(false)}
+            onClick={updateSlug}
           >
             Continue
             <icons.CaretRight />
@@ -68,4 +102,47 @@ function ChooseURL() {
       </div>
     </div>
   );
+}
+
+
+function ChooseAvailabilities() {
+  const [updating, setUpdating] = useState(false);
+  const updateAvailabilities = () => {};
+  const [fadingOut, setFadingOut] = useState(false);
+  const [fadingIn, setFadingIn] = useState(true);
+  const [timeZone, setTimeZone] = useState("");
+  useEffect(() => {
+    setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone);
+    setFadingIn(false);
+  }, []);
+  return (
+    <div
+    className={cx("absolute w-screen transition-500", {
+      "opacity-0": fadingOut || fadingIn,
+      "!-translate-x-5": fadingOut,
+      "!translate-x-5": fadingIn,
+    })}
+  >
+    <div className="mt-8 flex flex-col gap-4 border rounded-lg border-gray-600 py-8 px-8 max-w-lg mx-auto">
+      <h2 className="font-medium text-lg">Set your availability</h2>
+      <p className="text-gray-500">
+        Timezone: {timeZone}
+      </p>
+      <ul>
+        <li>TODO(kt3k): List Availability Ranges</li>
+      </ul>
+      <div className="self-end mt-4">
+        <Button
+          disabled={updating || true}
+          style="primary"
+          onClick={updateAvailabilities}
+        >
+          Continue
+          <icons.CaretRight />
+        </Button>
+      </div>
+    </div>
+  </div>
+
+  )
 }
