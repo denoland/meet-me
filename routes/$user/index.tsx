d157d208b642d1fbc6a064b6e3d15d7edaad0bdc
Yoshiya Hinosawa <stibium121@gmail.com>
2022-06-07 22:59:06 +0900

    feat: placeholder booking page

---
+++ routes/$user/index.tsx
@@ -0,0 +1,23 @@
+// Copyright 2022 the Deno authors. All rights reserved. MIT license.
+
+import { useData, useForwardProps } from "aleph/react";
+import { getUserBySlug, UserForClient as User } from "utils/db.ts";
+import { ok } from "utils/api.ts";
+
+export const data = {
+  async get(req: Request, ctx: Context) {
+    const slug = ctx.params.user;
+    const user = await getUserBySlug(slug);
+    // Passes only necessary info
+    return ok({ givenName: user?.givenName });
+  },
+};
+
+export default function () {
+  const { data } = useData<User>();
+  return (
+    <div className="max-w-screen-xl px-4 m-auto">
+      <h1>{data.givenName}'s booking page</h1>
+    </div>
+  );
+}
