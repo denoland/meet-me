b5ffb84aaab5f266595d63c58d053fbc11ee5896
Yoshiya Hinosawa <stibium121@gmail.com>
2022-06-10 22:32:02 +0900

    remove duplicated profile area

--- routes/mypage/index.tsx
+++ routes/mypage/index.tsx
@@ -24,17 +24,6 @@ export default function MyPage() {
 
   return (
     <div className="max-w-screen-xl px-4 m-auto">
-      <div className="mt-4 flex items-center gap-3">
-        <img src={user.picture} className="rounded-full h-10 w-10" />
-        <div className="flex flex-col gap-1 text-sm">
-          <p>{user.name}</p>
-          <p>
-            <a className="text-blue-400" href={`/${user.slug}`} target="_blank">
-              meet-me.deno.dev/{user.slug}
-            </a>
-          </p>
-        </div>
-      </div>
       <div className="mt-6 grid grid-cols-3 gap-3">
         {user.eventTypes!.map((et) => (
           <EventTypeCard
