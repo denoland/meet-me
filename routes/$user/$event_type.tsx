8cea1266769681cbb470a7ef9e66d28af3472ee3
Yoshiya Hinosawa <stibium121@gmail.com>
2022-06-20 14:21:31 +0900

    feat: add redirect from booking pages when the user is unavailable

--- routes/$user/$event_type.tsx
+++ routes/$user/$event_type.tsx
@@ -1,34 +1,61 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-import { useData, useForwardProps } from "aleph/react";
-import { EventType, getUserBySlug, Range } from "utils/db.ts";
-import { ok } from "utils/api.ts";
+import { useEffect } from "react";
+import { useData, useRouter } from "aleph/react";
+import { CALENDAR_FREE_BUSY_API, TOKEN_ENDPOINT } from "utils/const.ts";
+import { EventType, getUserAvailability, getUserBySlug } from "utils/db.ts";
 
 export const data = {
   async get(_req: Request, ctx: Context) {
     const slug = ctx.params.user;
     const eventId = ctx.params.event_type;
     const user = await getUserBySlug(slug);
+    if (!user) {
+      return Response.json({ error: { message: "User not found" } });
+    }
     const eventType = user?.eventTypes?.find((et) =>
       et.id === eventId || et.slug === eventId
     );
+    const availableRanges = await getUserAvailability(
+      user!,
+      new Date(),
+      new Date("2022-07-31"),
+      {
+        freeBusyApi: CALENDAR_FREE_BUSY_API,
+        tokenEndpoint: TOKEN_ENDPOINT,
+      },
+    );
     // Passes only necessary info
-    return ok({
+    return Response.json({
       givenName: user?.givenName,
-      availabilities: user?.availabilities,
+      availableRanges,
       eventType,
     });
   },
 };
 
 export default function () {
+  const { redirect } = useRouter();
   const { data } = useData<
-    { givenName: string; availabilities: Range[]; eventType: EventType }
+    {
+      givenName: string;
+      availableRanges: { start: string; end: string }[];
+      eventType: EventType;
+      error?: { message: string };
+    }
   >();
+  useEffect(() => {
+    if (data.error) {
+      redirect("/");
+    }
+  }, []);
+  if (data.error) {
+    return null;
+  }
   return (
     <div className="max-w-screen-xl px-4 m-auto">
       <h1>{data.givenName}'s booking page</h1>
-      <pre>{JSON.stringify(data.availabilities, null, 2)}</pre>
+      <pre>{JSON.stringify(data.availableRanges, null, 2)}</pre>
       <pre>{JSON.stringify(data.eventType, null, 2)}</pre>
     </div>
   );
--- routes/$user/index.tsx
+++ routes/$user/index.tsx
@@ -1,13 +1,17 @@
 // Copyright 2022 the Deno authors. All rights reserved. MIT license.
 
-import { useData, useForwardProps } from "aleph/react";
-import { getUserBySlug, UserForClient as User } from "utils/db.ts";
+import { useEffect } from "react";
+import { useData, useRouter } from "aleph/react";
+import { EventType, getUserBySlug } from "utils/db.ts";
 import { ok } from "utils/api.ts";
 
 export const data = {
   async get(_req: Request, ctx: Context) {
     const slug = ctx.params.user;
     const user = await getUserBySlug(slug);
+    if (!user) {
+      return Response.json({ error: { message: "User not found" } });
+    }
     // Passes only necessary info
     return ok({
       givenName: user?.givenName,
@@ -18,7 +22,25 @@ export const data = {
 };
 
 export default function () {
-  const { data } = useData<User>();
+  const { redirect } = useRouter();
+  const { data } = useData<
+    {
+      givenName: string;
+      slug: string;
+      eventTypes: EventType[];
+      error?: { message: string };
+    }
+  >();
+
+  useEffect(() => {
+    if (data.error) {
+      redirect("/");
+    }
+  }, []);
+  if (data.error) {
+    return null;
+  }
+
   return (
     <div className="max-w-screen-xl px-4 m-auto">
       <h1>{data.givenName}'s booking page</h1>
