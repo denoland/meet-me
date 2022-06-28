e74c94c4a3be0be875c25a29fbeb756025d29b16
Yoshiya Hinosawa <stibium121@gmail.com>
2022-06-28 17:22:48 +0900

    fix: restore footer links (#55)

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,27 +6,19 @@ export function Footer() {
   return (
     <footer className="sticky top-full max-w-screen-xl mx-auto mt-8 h-25 flex items-center gap-4 px-4 text-gray-300">
       <icons.Deno className="h-5 w-5" />
-      {
-        /* TODO(kt3k): Disable this link temporarily until showcase_cal made public
-        <a href="https://github.com/denoland/showcase_cal">
-          <span>Powered by Deno</span>
-        </a>*/
-      }
       <span>Powered by Deno</span>
-      {
-        /* TODO(kt3k): Disable this link temporarily until showcase_cal made public
-        ・
-        <a href="https://www.figma.com/file/P0XsTDIeiwNhm8jFS03gwz/Deno-Cal">
-          Figma
-        </a>*/
-      }
-      {
-        /* TODO(kt3k): Disable this link temporarily until showcase_cal made public
-        ・
-        <a href="https://github.com/denoland/showcase_cal/issues">
-          Issues
-        </a>*/
-      }
+      ・
+      <a href="https://github.com/denoland/meet-me">
+        Source
+      </a>
+      ・
+      <a href="https://www.figma.com/file/P0XsTDIeiwNhm8jFS03gwz/Deno-Cal">
+        Figma
+      </a>
+      ・
+      <a href="https://github.com/denoland/meet-me/issues">
+        Issues
+      </a>
       ・
       <a href="/terms">
         Terms
