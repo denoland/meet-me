4eb421f422ee7023ec04394227cd92cd5ec20937
Gledson <gledsoul@gmail.com>
2023-04-13 21:51:43 +0100

    Update index.tsx

---
+++ routes/index.tsx
@@ -0,0 +1,15 @@
+// Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
+/** @jsxImportSource https://esm.sh/react@18.2.0 */
+
+import { useEffect, useState } from "react";
+import { useForwardProps, useRouter } from "aleph/react";
+import icons from "icons";
+import cx from "utils/cx.ts";
+import { type User } from "utils/db.ts";
+
+export default function LandingPage() {
+  
+           
+}
+
+
