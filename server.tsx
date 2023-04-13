a1d5f593df86fa5c620937047995d5b8c2b3c756
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-13 22:24:14 +0100

    env cleaning

--- server.tsx
+++ server.tsx
@@ -2,29 +2,11 @@
 
 import presetUno from "@unocss/preset-uno";
 import { serve } from "aleph/react-server";
-import { initFirestore } from "utils/firestore.ts";
 import "std/dotenv/load.ts";
 
-// pre-import route modules for serverless env that doesn't support the dynamic imports.
+
 import routes from "./routes/_export.ts";
 
-initFirestore();
-
-const Signout: Middleware = {
-  fetch(req, ctx) {
-    const { pathname } = new URL(req.url);
-    if (pathname === "/signout") {
-      return new Response("", {
-        status: 303,
-        headers: {
-          location: "/",
-          "Set-Cookie": "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
-        },
-      });
-    }
-    return ctx.next();
-  },
-};
 
 serve({
   port: 3000,
