def50565fb38ec158d2ef4cafc12b5bb5b5239c7
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-13 22:40:16 +0100

    env cleaning

--- server.tsx
+++ server.tsx
@@ -1,28 +1,12 @@
+// Copyright 2022 the Deno authors. All rights reserved. MIT license.
+
 import presetUno from "@unocss/preset-uno";
 import { serve } from "aleph/react-server";
-import { initFirestore } from "utils/firestore.ts";
-import "std/dotenv/load.ts";
 
-// pre-import route modules for serverless env that doesn't support the dynamic imports.
+
+
 import routes from "./routes/_export.ts";
 
-initFirestore();
-
-const Signout: Middleware = {
-  fetch(req, ctx) {
-    const { pathname } = new URL(req.url);
-    if (pathname === "/signout") {
-      return new Response("", {
-        status: 303,
-        headers: {
-          location: "/",
-          "Set-Cookie": "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
-        },
-      });
-    }
-    return ctx.next();
-  },
-};
 
 serve({
   port: 3000,
@@ -41,6 +25,5 @@ serve({
       },
     },
   },
-  middlewares: [Signout],
   ssr: true,
 });
