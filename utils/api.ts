7e0ec288d07d9ddf49a088155af92c521a17234a
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-13 21:58:50 +0100

    env experiment

--- utils/api.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 the Deno authors. All rights reserved. MIT license.
-
-export function badRequest(message = "") {
-  return Response.json({ message }, { status: 400 });
-}
-
-export function ok(obj = {}) {
-  return Response.json(obj);
-}
--- utils/events.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 the Deno authors. All rights reserved. MIT license.
-
-import mitt from "https://esm.sh/mitt@3.0.0";
-
-// shared event emitter
-export default mitt<Record<string, Record<string, unknown>>>();
