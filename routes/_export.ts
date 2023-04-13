4f50eeecb747ee9505c3d51e1d272a92b11cbb89
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-13 22:42:52 +0100

    env cleaning

--- routes/_export.ts
+++ routes/_export.ts
@@ -1,1 +1,15 @@
-export default {}
+
+import * as $0 from "./_app.tsx";
+import * as $1 from "./todos.tsx";
+import * as $2 from "./index.tsx";
+import * as $3 from "./app.tsx";
+import * as $4 from "./_404.tsx";
+
+
+export default {
+  "/_app": $0,
+  "/todos": $1,
+  "/index": $2,
+  "/app": $3,
+  "/_404": $4,
+};
