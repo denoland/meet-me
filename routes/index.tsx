bd69f51e3313092be04fcce8106c507c969387a0
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-14 01:14:26 +0100

    cleaning

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,3 @@
 import { Head, Link } from "aleph/react";
 
-export default function Index() {
-  return (
-    <div className="screen index">
-      <Head>
-        <title>Wallet</title>
-        <meta name="description" content="gled-labs." />
-      </Head>
-      <p className="logo">
-        <img src="./assets/logo.svg" width="75" height="75" title="Aleph.js" />
-      </p>
-      <h1>
-        The Wallet.
-      </h1>
-      
-      <nav>
-        <Link
-          role="button"
-          to="/app">
-          Wallet
-        </Link>
-      </nav>
-    </div>
-  );
-}
+
