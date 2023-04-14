02836ed4213d97bdeb32fa5f34895560456a797c
Gledson <gledsoul@gmail.com>
2023-04-14 02:04:10 +0100

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,8 +3,7 @@ import Header from "../components/Header.tsx";
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
-      {children}
+     
     </>
   );
 }
